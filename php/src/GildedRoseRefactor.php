1969d66f8a067644f30833ecf016b67dc32a4290
Kévin Oliva <olivakevin64@gmail.com>
2021-12-17 15:56:41 +0100

    Création class GildedRose à refactorer et d'un test associé

--- php/src/GildedRoseRefactor.php
+++ php/src/GildedRoseRefactor.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace GildedRose;
 
-final class GildedRose
+final class GildedRoseRefactor
 {
     /**
      * @var Item[]
--- php/tests/GildedRoseTest.php
+++ php/tests/GildedRoseTest.php
@@ -4,28 +4,71 @@ declare(strict_types=1);
 
 namespace Tests;
 
+use ArrayObject;
 use GildedRose\GildedRose;
+use GildedRose\GildedRoseRefactor;
 use GildedRose\Item;
 use PHPUnit\Framework\TestCase;
 
 class GildedRoseTest extends TestCase
 {
-    public function testFoo(): void
-    {
-        $items = [new Item('foo', 0, 0)];
-        $gildedRose = new GildedRose($items);
-        $gildedRose->updateQuality();
-        $this->assertSame('foo', $items[0]->name);
-    }
+    // public function testFoo(): void
+    // {
+    //     $items = [new Item('foo', 0, 0)];
+    //     $gildedRose = new GildedRose($items);
+    //     $gildedRose->updateQuality();
+    //     $this->assertSame('foo', $items[0]->name);
+    // }
 
-    public function testAgedBrieOneDay(): void
+    // public function testAgedBrieOneDay(): void
+    // {
+    //     $items = [
+    //         new Item('Aged Brie', 2, 0),
+    //     ];
+    //     $gildedRose = new GildedRose($items);
+    //     $gildedRose->updateQuality();
+    //     $itemAfterUpdate = new Item('Aged Brie', 1, 1);
+    //     $this->assertEquals($items[0], $itemAfterUpdate);
+    // }
+
+
+    // Ici, nous supposons que la classe GildedRose est fonctionnelle, nous nous appuyons sur celle-ci pour tester
+    // la nouvelle classe que nous créons (GildedRoseRefactor) afin de vérifier si nos résultats sont toujours équivalents à 
+    // ceux de GildedRose
+    public function testOneDay(): void
     {
-        $items = [
+        // Nous créons deux tableaux car si nous faisons une copie du premier, les valeurs seront données par référence,
+        // cela nous empêchera de modifier un objet sans modifier l'autre
+        $itemsSafes = [
+            new Item('+5 Dexterity Vest', 10, 20),
             new Item('Aged Brie', 2, 0),
+            new Item('Elixir of the Mongoose', 5, 7),
+            new Item('Sulfuras, Hand of Ragnaros', 0, 80),
+            new Item('Sulfuras, Hand of Ragnaros', -1, 80),
+            new Item('Backstage passes to a TAFKAL80ETC concert', 15, 20),
+            new Item('Backstage passes to a TAFKAL80ETC concert', 10, 49),
+            new Item('Backstage passes to a TAFKAL80ETC concert', 5, 49),
         ];
-        $gildedRose = new GildedRose($items);
+
+        $itemsToTest = [
+            new Item('+5 Dexterity Vest', 10, 20),
+            new Item('Aged Brie', 2, 0),
+            new Item('Elixir of the Mongoose', 5, 7),
+            new Item('Sulfuras, Hand of Ragnaros', 0, 80),
+            new Item('Sulfuras, Hand of Ragnaros', -1, 80),
+            new Item('Backstage passes to a TAFKAL80ETC concert', 15, 20),
+            new Item('Backstage passes to a TAFKAL80ETC concert', 10, 49),
+            new Item('Backstage passes to a TAFKAL80ETC concert', 5, 49),
+        ];
+
+        $gildedRose = new GildedRose($itemsSafes);
+        $gildedRoseRefactor = new GildedRoseRefactor($itemsToTest);
+
         $gildedRose->updateQuality();
-        $itemAfterUpdate = new Item('Aged Brie', 1, 1);
-        $this->assertEquals($items[0], $itemAfterUpdate);
+        $gildedRoseRefactor->updateQuality();
+
+        for ($i=0; $i < sizeof($itemsSafes); $i++) { 
+            $this->assertEquals(($gildedRose->getItems())[$i], ($gildedRoseRefactor->getItems())[$i]);
+        }
     }
 }
